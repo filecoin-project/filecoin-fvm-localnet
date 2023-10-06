e6869bc46f21a2866dec1d5ec1d9fa52730a6f93
Taylor Hulsmans <thulsmans.1133@gmail.com>
2023-10-06 08:22:33 -0400

    working address grab

--- scripts/fillAccounts/index.js
+++ scripts/fillAccounts/index.js
@@ -2,19 +2,19 @@ require('dotenv').config()
 const child_process = require('child_process');
 const { ethers } = require('ethers')
 const lotusEvemStat = child_process.exec(`lotus`)
-console.log('mnemonic', process.env.MNEMONIC)
-//const hdNode  = ethers.HDNodeWallet.fromPhrase(process.env.MNEMONIC);
 const mnemonic = ethers.Mnemonic.fromPhrase(process.env.MNEMONIC);
 
 for (let index = 0; index < 10; index++) {
   //const  account = hdNode.derivePath(`m/44'/60'/0'/0/${index}`);
   const wallet = ethers.HDNodeWallet.fromMnemonic(mnemonic, `m/44'/60'/0'/0/${index}`)
   const stat = child_process.exec(`lotus evm stat ${wallet.address}`)
-  console.log(wallet.address);
   stat.stdout.on('data', (data) => {
     const addReg= /t410f\w+/
-    const addr = data.match(addReg)[1]
-    child_process.exec(`lotus send ${addr} 1000`)
+    const addr = data.match(addReg)
+    if (addr) {
+      child_process.exec(`lotus send ${addr[0]} 1000`)
+    }
+
 
   })
   stat.stderr.on('data', (data) => {
